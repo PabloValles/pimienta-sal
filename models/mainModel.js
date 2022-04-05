791669af251743d9f1fbb1fe232bc2c326a53b70
Pablo Valles <valles.pablo@hotmail.com>
2022-04-05 20:55:20 -0300

    Implementando modelos

---
+++ models/mainModel.js
@@ -0,0 +1,44 @@
+let mainModel = {
+  about: {
+    titulo: "Pimienta & Sal",
+    descripcion:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute iruredolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.We only use seasonal ingredients.",
+    historia:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  menus: [
+    {
+      idMenuDB: 0,
+      nombreMenuDB: "Carpaccio fresco",
+      descripcionCorta: "Entrada Carpaccio de salmón con cítricos",
+      descripcionDetallada:
+        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      precioMenuDB: 65.5,
+      imgMenuDB: "Carpaccio-de-salmon.jpg",
+    },
+    {
+      idMenuDB: 1,
+      nombreMenuDB: "Risotto de berenjena",
+      descripcionDetallada: "Risotto de berenjena y queso de cabra",
+      precioMenuDB: 47.0,
+      imgMenuDB: "Risotto-berenjena-queso-cabra.jpg",
+    },
+    {
+      idMenuDB: 2,
+      nombreMenuDB: "Mousse de arroz",
+      descripcionDetallada: "Mousse de arroz con leche y aroma de azahar",
+      precioMenuDB: 27.5,
+      imgMenuDB: "Mousse-de-arroz-con-leche.jpg",
+    },
+    {
+      idMenuDB: 3,
+      nombreMenuDB: "Espárragos blancos",
+      descripcionDetallada:
+        "Espárragos blancos con vinagreta de verduras y jamón ibérico",
+      precioMenuDB: 37.5,
+      imgMenuDB: "esparragos.png",
+    },
+  ],
+};
+
+module.exports = mainModel;
